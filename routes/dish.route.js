aaa710e7d25064c7c675d11a42f019880dbdbdfb
ttuyen <totuyencv@gmail.com>
2024-10-26 15:03:48 +0700

    Update dish controller

--- routes/dish.route.js
+++ routes/dish.route.js
@@ -1,6 +1,5 @@
 import express from "express";
 import DishController from "../controllers/dish.controller.js";
-import upload from "../middlewares/upload.js";
 
 const router = express.Router();
 
@@ -8,8 +7,8 @@ const dishController = new DishController();
 
 router.get("/dishes", dishController.getAllDishes);
 router.get("/dishes/:id", dishController.getDishDetail);
-router.post("/dishes", upload.array("images", 10), dishController.createDish);
-router.put("/dishes/:id", upload.array("images", 10), dishController.updateDish);
+router.post("/dishes", dishController.createDish);
+router.put("/dishes/:id", dishController.updateDish);
 router.delete("/dishes/:id", dishController.deleteDish);
 
 export default router;
