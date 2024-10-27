ec28164befec5979ece393bccdb5d5930b3e260d
ttuyen <totuyencv@gmail.com>
2024-10-27 18:14:33 +0700

    Update dish controller

--- controllers/dish.controller.js
+++ controllers/dish.controller.js
@@ -1,5 +1,4 @@
 import Dish from "../models/dish.js";
-import cloudinary from "../configs/cloudinary.js";
 
 class DishController {
   async getAllDishes(req, res) {
@@ -43,39 +42,6 @@ class DishController {
     }
   }
 
-  // async createDish(req, res) {
-  //   try {
-  //     // Kiểm tra nếu có file được upload
-  //     if (!req.files || req.files.length === 0) {
-  //       return res.status(400).json({ message: "No image files provided" });
-  //     }
-
-  //     // Upload từng file lên Cloudinary và lưu URL vào mảng
-  //     const uploadPromises = req.files.map((file) =>
-  //       cloudinary.uploader.upload(file.path, {
-  //         folder: "RM",
-  //       })
-  //     );
-
-  //     const results = await Promise.all(uploadPromises);
-  //     const imageUrls = results.map((result) => result.secure_url); // Lấy URL
-
-  //     // Tạo món ăn mới
-  //     const newDish = new Dish({
-  //       ...req.body,
-  //       images: imageUrls, // Lưu mảng URL ảnh từ Cloudinary vào cơ sở dữ liệu
-  //     });
-
-  //     await newDish.save();
-
-  //     return res.status(201).json(newDish);
-  //   } catch (error) {
-  //     return res
-  //       .status(500)
-  //       .json({ message: "Create dish failed", error: error.message });
-  //   }
-  // }
-
   async createDish(req, res) {
     try {
       const existingProduct = await Dish.findOne({ name: req.body.name });
@@ -105,7 +71,10 @@ class DishController {
 
   async updateDish(req, res) {
     try {
-      const existingProduct = await Dish.findOne({ name: req.body.name });
+      const existingProduct = await Dish.findOne({
+        name: req.body.name,
+        _id: { $ne: req.params.id }, // trừ id món ăn hiện tại tránh err khi update mà không thay đổi tên
+      });
       if (existingProduct) {
         return res.status(400).json({
           message: "The product name already exists",
@@ -116,7 +85,6 @@ class DishController {
         ? req.files.map((file) => file.path)
         : req.body.images;
 
-      // Tạo object chứa thông tin cần cập nhật
       const updateData = {
         ...req.body,
         images,
