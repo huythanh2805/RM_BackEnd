ccaeaead32ec8750f9a42e5e840d0dd53ddcea4d
ttuyen <totuyencv@gmail.com>
2024-11-25 17:01:41 +0700

    Add feedback controller

---
+++ controllers/feedback.controller.js
@@ -0,0 +1,74 @@
+import Feedback from "../models/feedback.js";
+import message from "../models/message.js";
+
+class FeedbackController {
+  async getAllFeedback(req, res) {
+    try {
+      const feedbacks = await Feedback.find({});
+
+      if (feedbacks.length === 0) {
+        return res.status(404).json({
+          message: "No feedbacks found",
+        });
+      }
+
+      return res.status(200).json({ feedbacks });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Get all feedbacks failed",
+        error: error.message,
+      });
+    }
+  }
+
+  async getFeedbackByDishId(req, res) {
+    const { id } = req.params;
+
+    try {
+      const feedbacks = await Feedback.find({ dish_id: id });
+
+      if (feedbacks.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "No feedbacks found for this dish" });
+      }
+
+      return res.status(200).json({ feedbacks });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Get feedbacks by dish ID failed",
+        error: error.message,
+      });
+    }
+  }
+
+  async createFeedback(req, res) {
+    try {
+      const feedback = await Feedback.create(req.body);
+
+      return res.status(201).json({ feedback });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Create feedback failed",
+        error: error.message,
+      });
+    }
+  }
+
+  async deleteFeedback(req, res) {
+    try {
+      const feedback = await Feedback.findByIdAndDelete(req.params.id);
+
+      return res.status(200).json({
+        message: "Delete successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Delete feedback failed",
+        error: error.message,
+      });
+    }
+  }
+}
+
+export default FeedbackController;
