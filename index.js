477ad590e30bbb8b88e8a4357f169c214e9f5013
Uyenlun113 <thiuyen1132004@gmail.com>
2024-12-19 21:19:18 +0700

    update

--- index.js
+++ index.js
@@ -86,11 +86,11 @@ app.use("/api", paymentRoute);
 
 app.use(feedbackRoute);
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // All routes should return the index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 //cron
 startCronJob();
