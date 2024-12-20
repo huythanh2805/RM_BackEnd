54a6dddfdb77e5c1ebae215cd197d25a78cd591b
Uyenlun113 <thiuyen1132004@gmail.com>
2024-12-20 08:56:32 +0700

    update

--- controllers/bill.controller.js
+++ controllers/bill.controller.js
@@ -269,7 +269,7 @@ class BillController {
       // Create a new notification
       const notification = new notifications({
         title: "Thanh toán thành công",
-        message: `Đơn đặt bàn ${transactionCode} đã được thanh toán thành công.`,
+        message: `Đơn đặt bàn ${transactionCode} đã được thanh toán thành công. Vui lòng kiểm tra`,
       });
 
       await notification.save();
